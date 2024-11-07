c759c4bf234c73790f054746678b2ecf340046a4
Mahynlo <malcom_navarro12@hotmail.com>
2024-11-06 22:33:57 -0700

    Modificacion de titulo

--- index.php
+++ index.php
@@ -11,25 +11,25 @@
 
 <body>
 
-    <h1>Yo modifique esto<h1>
-            <h2>Este es una prueba<h2>
+    <h1>Yo modifique esto y el conficto esta solucionado<h1>
+    <h2>Este es una prueba<h2>
 
-                    <div>
-                        <h1>este es un contenedor y no tine dada<h1>
-                        <h2>Dos<h2>
-                        <h1>hola tu como te encuentras perre flor de casa</h1>
-                        <a href="https://www.google.com">Google</a>
-                    </div>
+            <div>
+                <h1>este es un contenedor y no tine dada<h1>
+                <h2>Dos<h2>
+                <h1>hola tu como te encuentras perre flor de casa</h1>
+                <a href="https://www.google.com">Google</a>
+            </div>
 
 
-                    <div>
-                        <h1>No se esto lo camie por otra cosa<h1>
-                        <h2>Dos<h2>
-                        <h2>Tres no es problema <h2>
-                        <h1>hola tu como te encuentras perre flor de casa</h1>
-                        <a href="https://www.google.com">Google</a>
-                        <a href="https://www.facebook.com">Facebook</a>
-                    </div>
+            <div>
+                <h1>No se esto lo camie por otra cosa<h1>
+                <h2>Dos<h2>
+                <h2>Tres no es problema <h2>
+                <h1>hola tu como te encuentras perre flor de casa</h1>
+                <a href="https://www.google.com">Google</a>
+                <a href="https://www.facebook.com">Facebook</a>
+            </div>
 
 </body>
 
