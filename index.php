efed8a9a1cdda5b889a7fe03933e17126f5c0969
Mahynlo <malcom_navarro12@hotmail.com>
2024-11-06 21:52:30 -0700

    Modificacion a la estructura

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L pruebas de la verdad </title>
-    
-    <h1>Yo modifique esto<h1>
-    <h2>Este es una prueba<h2>
-
-        <div><h1>este es un contenedor y  no tine dada<h1>
-            <h2>Dos<h2>
-        </div>
-
-
-        <div><h1>este es un contenedor y  no tine dada<h1>
-            <h2>Dos<h2>
-        <h1>hola tu como te encuentras perre flor de casa</h1>
 
 </head>
+
 <body>
-    
+
+    <h1>Yo modifique esto<h1>
+            <h2>Este es una prueba<h2>
+
+                    <div>
+                        <h1>este es un contenedor y no tine dada<h1>
+                                <h2>Dos<h2>
+                    </div>
+
+
+                    <div>
+                        <h1>No se esto lo camie por otra cosa<h1>
+                                <h2>Dos<h2>
+                                        <h1>hola tu como te encuentras perre flor de casa</h1>
+                    </div>
+
 </body>
+
 </html>
